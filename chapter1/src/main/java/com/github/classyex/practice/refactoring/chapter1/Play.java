dcfc4309533e4d472b0b257ce21221d5fd7cd2a0
classyex <classyex@qq.com>
2020-07-09 15:12:30 +0800

    添加json转对象

---
+++ chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/Play.java
@@ -0,0 +1,17 @@
+package com.github.classyex.practice.refactoring.chapter1;
+
+import lombok.Data;
+
+/**
+* Play.java <br>
+* @version 1.0 <br>
+* @date 2020/7/9 15:08 <br>
+* @author yex <br>
+*/
+@Data
+public class Play {
+
+    private String name;
+    private String type;
+
+}
--- chapter1/src/test/java/com/github/classyex/practice/refactoring/chapter1/PlayTest.java
+++ chapter1/src/test/java/com/github/classyex/practice/refactoring/chapter1/PlayTest.java
@@ -1,11 +1,14 @@
 package com.github.classyex.practice.refactoring.chapter1;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -13,13 +16,22 @@ import static org.hamcrest.Matchers.is;
 
 public class PlayTest {
 
+    public static final String PLAYS_STR = "{\"hamlet\":{\"name\":\"Hamlet\",\"type\":\"tragedy\"},\"as-like\":{\"name\":\"As You Like It\",\"type\":\"comedy\"},\"othello\":{\"name\":\"Othello\",\"type\":\"tragedy\"}}";
+
     @Test
     public void given_plays_string_when_json_format_then_get_value_correct() throws JsonProcessingException {
-        String playsStr = "{\"hamlet\":{\"name\":\"Hamlet\",\"type\":\"tragedy\"},\"as-like\":{\"name\":\"As You Like It\",\"type\":\"comedy\"},\"othello\":{\"name\":\"Othello\",\"type\":\"tragedy\"}}";
-        JsonNode jsonNode = new ObjectMapper().readTree(playsStr);
+        JsonNode jsonNode = new ObjectMapper().readTree(PLAYS_STR);
         assertThat(jsonNode.size(), is(equalTo(3)));
         assertThat(jsonNode.get("as-like").get("name").asText(), is(equalTo("As You Like It")));
         assertThat(jsonNode.get("as-like").get("type").asText(), is(equalTo("comedy")));
+    }
+
+    @Test
+    public void given_plays_string_when_format_bean_then_correct() throws JsonProcessingException {
+        Map<String, Play> playMap = new ObjectMapper().readValue(PLAYS_STR, new TypeReference<Map<String, Play>>() {});
+        assertThat(playMap.size(), is(equalTo(3)));
+        assertThat(playMap.get("as-like").getName(), is(equalTo("As You Like It")));
+        assertThat(playMap.get("as-like").getType(), is(equalTo("comedy")));
     }
 
 
