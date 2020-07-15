88536e17d3b8a6948396468a312860cef4d9eb21
classyex <classyex@qq.com>
2020-07-15 14:57:28 +0800

    搬移amountFor函数到PerformanceCalculator

--- chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/PerformanceCalculator.java
+++ chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/PerformanceCalculator.java
@@ -22,4 +22,36 @@ public class PerformanceCalculator {
     }
 
 
+    int amountFor() {
+        int result = 0;
+
+        switch (play.getType()) {
+            case "tragedy":
+                final int tragedyBaseAmount = 40000;
+                result = tragedyBaseAmount;
+                final int tragedyAudienceBase = 30;
+                if (performance.getAudience() > tragedyAudienceBase) {
+                    final int tragedyOverPerAmount = 1000;
+                    result += tragedyOverPerAmount * (performance.getAudience() - tragedyAudienceBase);
+                }
+                break;
+            case "comedy":
+                final int comedyBaseAmount = 30000;
+                result = comedyBaseAmount;
+                final int comedyAudienceBase = 20;
+                if (performance.getAudience() > comedyAudienceBase) {
+                    final int comedyOverPerAmount = 500;
+                    final int comedyOverBaseAmount = 10000;
+                    result += comedyOverBaseAmount + comedyOverPerAmount * (
+                            performance.getAudience() - comedyAudienceBase);
+                }
+                final int comedyFactory = 300;
+                result += comedyFactory * performance.getAudience();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("unknown type: %s", play.getType()));
+        }
+        return result;
+    }
+
 }
