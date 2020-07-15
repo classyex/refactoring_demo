17646119d3ded89edcc75e6e83b1f141805ad375
classyex <classyex@qq.com>
2020-07-15 13:40:41 +0800

    移动代码顺序，使其与1.8初始代码一致

--- chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/StatementData.java
+++ chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/StatementData.java
@@ -30,24 +30,8 @@ public class StatementData {
         return performances;
     }
 
-    int totalAmount() {
-        return getPerformances().stream().map(this::amountFor).reduce(0, Integer::sum);
-    }
-
-    int totalVolumeCredits() {
-        return getPerformances().stream().map(this::volumeCreditsFor).reduce(0, Integer::sum);
-    }
-
-    private int volumeCreditsFor(final Performance aPerformance) {
-        final int creditsBase = 30;
-        final int defaultCredits = 0;
-        int result = Math.max(aPerformance.getAudience() - creditsBase, defaultCredits);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(playFor(aPerformance).getType())) {
-            final float comedyExtraCreditPer = 5.0F;
-            result += Math.floor(aPerformance.getAudience() / comedyExtraCreditPer);
-        }
-        return result;
+    Play playFor(final Performance aPerformance) {
+        return plays.get(aPerformance.getPlayID());
     }
 
     int amountFor(final Performance aPerformance) {
@@ -82,8 +66,25 @@ public class StatementData {
         return result;
     }
 
-    Play playFor(final Performance aPerformance) {
-        return plays.get(aPerformance.getPlayID());
+    private int volumeCreditsFor(final Performance aPerformance) {
+        final int creditsBase = 30;
+        final int defaultCredits = 0;
+        int result = Math.max(aPerformance.getAudience() - creditsBase, defaultCredits);
+        // add extra credit for every ten comedy attendees
+        if ("comedy".equals(playFor(aPerformance).getType())) {
+            final float comedyExtraCreditPer = 5.0F;
+            result += Math.floor(aPerformance.getAudience() / comedyExtraCreditPer);
+        }
+        return result;
     }
+
+    int totalAmount() {
+        return getPerformances().stream().map(this::amountFor).reduce(0, Integer::sum);
+    }
+
+    int totalVolumeCredits() {
+        return getPerformances().stream().map(this::volumeCreditsFor).reduce(0, Integer::sum);
+    }
+
 
 }
