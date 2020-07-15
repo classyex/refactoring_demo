4082b77e9b87bcd7fa91ccf5302a7aa74f8f4943
classyex <classyex@qq.com>
2020-07-15 15:53:51 +0800

    整理喜剧子类方法

--- chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/ComedyPerformanceCalculator.java
+++ chapter1/src/main/java/com/github/classyex/practice/refactoring/chapter1/ComedyPerformanceCalculator.java
@@ -34,15 +34,7 @@ public class ComedyPerformanceCalculator extends PerformanceCalculator {
 
     @Override
     int volumeCreditsFor() {
-
-        final int creditsBase = 30;
-        final int defaultCredits = 0;
-        int result = Math.max(performance.getAudience() - creditsBase, defaultCredits);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(performance.getPlay().getType())) {
-            final float comedyExtraCreditPer = 5.0F;
-            result += Math.floor(performance.getAudience() / comedyExtraCreditPer);
-        }
-        return result;
+        final float comedyExtraCreditPer = 5.0F;
+        return (int) (super.volumeCreditsFor() + Math.floor(performance.getAudience() / comedyExtraCreditPer));
     }
 }
