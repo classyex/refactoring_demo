de2bf0bf3b9dc51345731823a5a8d4647216c146
classyex <classyex@qq.com>
2020-07-18 17:09:09 +0800

    添加第四章

---
+++ chapter4/src/main/java/com/github/classyex/practice/refactoring/chapter4/Province.java
@@ -0,0 +1,14 @@
+package com.github.classyex.practice.refactoring.chapter4;
+
+/**
+* Province.java <br>
+* @version 1.0 <br>
+* @date 2020/7/18 17:03 <br>
+* @author yex <br>
+*/
+
+public class Province {
+
+
+
+}
